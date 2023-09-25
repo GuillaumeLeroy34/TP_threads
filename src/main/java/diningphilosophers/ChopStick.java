2584d5da01f64ae9bdc894d3fb86ffa939e2181c
GuillaumeLeroy <guillaume.leroy34@gmail.com>
2023-09-25 21:04:16 +0200

    changements finaux

--- src/main/java/diningphilosophers/ChopStick.java
+++ src/main/java/diningphilosophers/ChopStick.java
@@ -1,18 +1,26 @@
 package diningphilosophers;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.Condition;
 public class ChopStick {
 
     private static int stickCount = 0;
     private boolean iAmFree = true;
     private final int myNumber;
+    private final Lock verrou = new ReentrantLock();
+    private final Condition deuxDispos = verrou.newCondition();
 
     public ChopStick() {
         myNumber = ++stickCount;
     }
 
-    synchronized public boolean tryTake(int delay) throws InterruptedException {
+    synchronized public boolean tryTake(long delay) throws InterruptedException {
+        verrou.lock();
+        try{
         if (!iAmFree) {
-            wait(delay);
+            deuxDispos.await(delay, TimeUnit.MILLISECONDS);
             if (!iAmFree) // Toujours pas libre, on abandonne
             {
                 return false; // Echec
@@ -21,12 +29,17 @@ public class ChopStick {
         iAmFree = false;
         // Pas utile de faire notifyAll ici, personne n'attend qu'elle soit occupée
         return true; // Succès
+        } finally{verrou.unlock();}
     }
 
     synchronized public void release() {
-        iAmFree = true;
-        notifyAll();
+        verrou.lock();
+        try{
+            iAmFree = true;
+        deuxDispos.signalAll();
         System.out.println("Stick " + myNumber + " Released");
+    }
+    finally{verrou.unlock();}
     }
 
     @Override
